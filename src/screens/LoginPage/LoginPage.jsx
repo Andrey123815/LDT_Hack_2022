595184d4b398446d73b0f5d44bfacf574a8be353
Andrey123815 <ivredomon@gmail.com>
2022-10-25 21:31:24 +0300

    login page refactoring

--- src/screens/LoginPage/LoginPage.jsx
+++ src/screens/LoginPage/LoginPage.jsx
@@ -3,18 +3,22 @@ import Input from "../../UI-KIT/Input/Input.jsx";
 import SimpleButton from "../../UI-KIT/SimpleButton/SimpleButton.jsx";
 import SupportOffer from "../../UI-KIT/SupportOffer/SupportOffer.jsx";
 import {NavLink} from "react-router-dom";
+import './LoginPage.scss';
 
 function LoginPage() {
+  const width = '85%';
   return (
-    <>
-      <img style={{marginTop: "15%", marginBottom: "10%"}} src="vdnh.svg" width="55%"  alt="ВДНХ"/>
-      <Input placeholder="Введите свой телефон" width="85%" />
-      <Input placeholder="Введите свой телефон" width="85%" />
-      <SimpleButton width="85%" marginTop="4%">
-        <NavLink to={'/route-selection'}>Войти</NavLink>
-      </SimpleButton>
-      <SupportOffer title="Забыли пароль?" />
-    </>
+    <div className="login-page">
+      <div className="login-page__content">
+        <img style={{marginTop: "15%", marginBottom: "10%"}} src="vdnh.svg" width="55%" alt="ВДНХ"/>
+        <Input placeholder="Телефон или e-mail" width={width}/>
+        <Input placeholder="Пароль" width={width}/>
+        <SimpleButton width={width} marginTop="4%">
+          <NavLink to={'/route-selection'}>Войти</NavLink>
+        </SimpleButton>
+        <SupportOffer title="Забыли пароль?"/>
+      </div>
+    </div>
   );
 }
 
