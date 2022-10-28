1fb37025ab9d9047d902ae7fd3b0787634cb4cb2
Andrey123815 <ivredomon@gmail.com>
2022-10-28 22:47:43 +0300

    delete trash components, refactroging navigation

--- src/UI-KIT/SimpleButton/SimpleButton.jsx
+++ src/UI-KIT/SimpleButton/SimpleButton.jsx
@@ -3,8 +3,8 @@ import './SimpleButton.scss';
 
 function SimpleButton(props) {
   return (
-    <button className="simple-button" style={{width: props.width, marginTop: props.marginTop}}>
-      {props.children}
+    <button className="simple-button" style={{width: props.width, marginTop: props.marginTop}} onClick={props.onClick}>
+      <span>{props.children}</span>
     </button>
   );
 }
--- src/screens/LoginPage/LoginPage.jsx
+++ src/screens/LoginPage/LoginPage.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Input from "../../UI-KIT/Input/Input.jsx";
 import SimpleButton from "../../UI-KIT/SimpleButton/SimpleButton.jsx";
-import SupportOffer from "../../UI-KIT/SupportOffer/SupportOffer.jsx";
-import {NavLink} from "react-router-dom";
 import './LoginPage.scss';
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const width = '85%';
+  const navigate = useNavigate();
   return (
     <div className="login-page">
       <div className="login-page__content">
         <img style={{marginTop: "15%", marginBottom: "10%"}} src="vdnh.svg" width="55%" alt="ВДНХ"/>
         <Input placeholder="Телефон или e-mail" width={width}/>
         <Input placeholder="Пароль" width={width}/>
-        <SimpleButton width={width} marginTop="4%">
-          <NavLink to={'/route-selection'}>Войти</NavLink>
+        <SimpleButton width={width} marginTop="4%" onClick={() => navigate('/route-selection')}>
+          Войти
         </SimpleButton>
-        <SupportOffer title="Забыли пароль?"/>
       </div>
     </div>
   );
